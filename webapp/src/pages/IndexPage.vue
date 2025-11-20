66c0fae9168bd2b87ad686168559e025c90af421
asmol10 <asmolic@veleri.hr>
2025-11-20 14:01:24 +0100

    test2

--- webapp/src/pages/IndexPage.vue
+++ webapp/src/pages/IndexPage.vue
@@ -1,13 +1,40 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+  <q-page padding>
+    <!-- Input field -->
+    <q-input 
+      rounded 
+      outlined 
+      v-model="naziv" 
+      label="Bike name" 
     />
+
+    <!-- Button to trigger printing -->
+    <q-btn 
+      label="Print Bike Name" 
+      color="primary" 
+      @click="printName"
+      class="q-mt-md"
+    />
+
+    <!-- Display the value -->
+    <div class="q-mt-md">
+      <strong>Bike Name:</strong> {{ nazivDisplay }}
+    </div>
   </q-page>
 </template>
 
 <script setup>
-//
+import { ref } from 'vue'
+
+// Reactive variable bound to input
+const naziv = ref('')
+
+// Variable to store printed output
+const nazivDisplay = ref('')
+
+// Function to “print” the name
+function printName() {
+  nazivDisplay.value = naziv.value
+  console.log('Bike name:', naziv.value) // also prints to browser console
+}
 </script>
